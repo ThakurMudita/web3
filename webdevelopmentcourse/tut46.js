1b6bfcf7796b360ebd16dc4af05fb1378fbad865
ThakurMudita <muditathakur570@gmail.com>
2023-10-11 12:14:25 +0530

    test

---
+++ webdevelopmentcourse/tut46.js
@@ -0,0 +1,16 @@
+let name='Mudita';
+let name1='shashi';
+let name2='Prachi';
+let name3='Umang';
+let surname='thakur';
+function greet(name,surname){
+    console.log(name + ' is a good student')
+    console.log(surname + 'royal')
+}
+greet(name1,surname);
+greet(name2,surname);
+greet(name3,surname);
+// console.log(name +" is a good girl");
+// console.log(name1 +" is a good girl");
+// console.log(name2 +" is a good girl");
+// console.log(name3 +" is a good boy");
